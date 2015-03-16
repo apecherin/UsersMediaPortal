8556fc080a668b86e62558d2144788727f652565
apecherin <artempecherin@gmail.com>
2015-03-16 23:35:13 +0300

    prepare

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :nickname) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :current_password, :password, :password_confirmation, :nickname) }
+  end
+
   protect_from_forgery with: :exception
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,46 @@
 class User
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  include Mongoid::Timestamps
+  def as_json(options={})
+    attrs = super(options)
+    attrs["id"] = attrs["_id"]
+    attrs
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  ## Database authenticatable
-  field :email,              type: String, default: ""
+  validates :username_validations, presence: true
+
+  field :email, type: String, default: ""
+  field :username, type: String, default: ""
+
   field :encrypted_password, type: String, default: ""
+  field :current_password, type: String, default: ""
 
   ## Recoverable
-  field :reset_password_token,   type: String
+  field :reset_password_token, type: String
   field :reset_password_sent_at, type: Time
 
   ## Rememberable
   field :remember_created_at, type: Time
 
   ## Trackable
-  field :sign_in_count,      type: Integer, default: 0
-  field :current_sign_in_at, type: Time
-  field :last_sign_in_at,    type: Time
-  field :current_sign_in_ip, type: String
-  field :last_sign_in_ip,    type: String
+  field :sign_in_count,       type: Integer, default: 0
+  field :current_sign_in_at,  type: Time
+  field :last_sign_in_at,     type: Time
+  field :current_sign_in_ip,  type: String
+  field :last_sign_in_ip,     type: String
 
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
+  def username_validations
+    errors.add(:username, 'can\'t be empty') if username.blank?
+    errors.add(:username, 'can\'t contain less than 3 symbols') if username.size < 3
+    username = self.username.split
+    errors.add(:username, 'can\'t contain more than 2 words') if username.size > 2
+    if username.size == 2
+      errors.add(:username, '1 part can\'t contain more than 12 symbols') if username[0].size > 12
+      errors.add(:username, '2 part can\'t contain more than 12 symbols') if username[1].size > 12
+    elsif username.size == 1
+      errors.add(:username, 'can\'t contain more than 12 symbols') if username[0].size > 12
+    end
+  end
 end
